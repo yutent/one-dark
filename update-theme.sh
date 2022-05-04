ba41bf538c70f1cd37c863c3e85ebdb6f559d1a7
宇天 <yutent.io@gmail.com>
2022-05-05 00:11:07 +0800

    init

---
+++ update-theme.sh
@@ -0,0 +1,16 @@
+
+
+# 控件主题
+gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y"
+
+# 鼠标主题
+gsettings set org.cinnamon.desktop.interface cursor-theme "Adwaita"
+
+# 图标主题
+gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y"
+
+# 桌面主题
+gsettings set org.cinnamon.theme name "Mint-Y"
+
+# 窗口主题
+gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y"
