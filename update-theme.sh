c3c0f88eb457af0eebd423dfc2d7ad0197ad9149
yutent <yutent.io@gmail.com>
2022-06-07 15:36:32 +0800

    update

--- update-theme.sh
+++ update-theme.sh
@@ -14,15 +14,15 @@ gsettings set org.cinnamon.desktop.interface gtk-theme "one-dark"
 # gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y"
 
 # 桌面主题
-# echo "重置桌面主题……"
-# gsettings set org.cinnamon.theme name "Mint-Y"
-# echo "重置完成, 0.5秒之后, 修改为one-dark"
-# sleep 0.5
-# gsettings set org.cinnamon.theme name "one-dark"
+echo "重置桌面主题……"
+gsettings set org.cinnamon.theme name "Mint-Y"
+echo "重置完成, 0.5秒之后, 修改为one-dark"
+sleep 0.5
+gsettings set org.cinnamon.theme name "one-dark"
 
 # 窗口主题
-# echo "重置窗口主题……"
-# gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y"
-# echo "重置完成, 0.5秒之后, 修改为one-dark"
-# sleep 0.5
-# gsettings set org.cinnamon.desktop.wm.preferences theme "one-dark"
+echo "重置窗口主题……"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y"
+echo "重置完成, 0.5秒之后, 修改为one-dark"
+sleep 0.5
+gsettings set org.cinnamon.desktop.wm.preferences theme "one-dark"
