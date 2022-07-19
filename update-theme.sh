1493baaf5f967c8dee50d15ef2b58748d234b930
yutent <yutent.io@gmail.com>
2022-07-19 19:59:05 +0800

    fixed popup item

--- update-theme.sh
+++ update-theme.sh
@@ -1,11 +1,11 @@
 
 
 # 控件主题(Controls)
-echo "重置控件主题……"
-gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y"
-echo "重置完成, 0.5秒之后, 修改为one-dark"
-sleep 0.5
-gsettings set org.cinnamon.desktop.interface gtk-theme "one-dark"
+# echo "重置控件主题……"
+# gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y"
+# echo "重置完成, 0.5秒之后, 修改为one-dark"
+# sleep 0.5
+# gsettings set org.cinnamon.desktop.interface gtk-theme "one-dark"
 
 # 鼠标主题(Mouse Pointer)
 # gsettings set org.cinnamon.desktop.interface cursor-theme "Adwaita"
@@ -14,11 +14,11 @@ gsettings set org.cinnamon.desktop.interface gtk-theme "one-dark"
 # gsettings set org.cinnamon.desktop.interface icon-theme "Reversal-green-dark"
 
 # 桌面主题(Desktop)
-# echo "重置桌面主题……"
-# gsettings set org.cinnamon.theme name "Mint-Y"
-# echo "重置完成, 0.5秒之后, 修改为one-dark"
-# sleep 0.5
-# gsettings set org.cinnamon.theme name "one-dark"
+echo "重置桌面主题……"
+gsettings set org.cinnamon.theme name "Mint-Y"
+echo "重置完成, 0.5秒之后, 修改为one-dark"
+sleep 0.5
+gsettings set org.cinnamon.theme name "one-dark"
 
 # 窗口主题(Windows borders)
 # echo "重置窗口主题……"
